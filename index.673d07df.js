3997d507300b0375c6c183f433d2aaeca0225307
Aleksandor2138 <99901672+Aleksandor2138@users.noreply.github.com>
2022-06-12 13:04:52 +0000

    Deploying to gh-pages from @ Aleksandor2138/IceCream@b39e69b82dafa005921f9cb11258d7d3cd7e8036 🚀

---
+++ index.673d07df.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-mob-menu-open]"),t=document.querySelector("[data-menu]");document.querySelector("[data-mob-menu-close]").addEventListener("click",(function(e){t.classList.remove("is-open")})),e.addEventListener("click",(function(){var n="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!n),t.classList.add("is-open")}))}();
+//# sourceMappingURL=index.673d07df.js.map

d0986ce1dd5192440cde910ea94b6dccc20bedcc
Mylymuk Oksana <oksana.mylymukk@gmail.com>
2022-06-14 12:28:10 +0300

    fix-modals

--- src/js/modal-1.js
+++ src/js/modal-1.js
@@ -1,20 +1,18 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-1-open]"),
-      openModalBtnheader: document.querySelector("[data-modal-1-open-header]"),
-      closeModalBtn: document.querySelector("[data-modal-1-close]"),
-      modal: document.querySelector("[data-modal-1]"),
-     
-      
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-    refs.openModalBtnheader.addEventListener("click", toggleModal);
-    
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-     
-    }
-  })();
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-1-open]'),
+    openModalBtnheader: document.querySelector('[data-modal-1-open-header]'),
+    closeModalBtn: document.querySelector('[data-modal-1-close]'),
+    modal: document.querySelector('[data-modal-1]'),
+    body: document.querySelector('body'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtnheader.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+})();
